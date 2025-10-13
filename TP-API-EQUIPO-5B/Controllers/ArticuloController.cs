aec83726ee67346adf028f0bc95f7e5f00189bdb
brunobberti <brunoberti094@gmail.com>
2025-10-13 17:20:25 -0300

    Se hizo el modificar

--- TP-API-EQUIPO-5B/Controllers/ArticuloController.cs
+++ TP-API-EQUIPO-5B/Controllers/ArticuloController.cs
@@ -71,13 +71,27 @@ namespace TP_API_EQUIPO_5B.Controllers
         }
 
         // PUT: api/Articulo/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]ArticuloDto articulo)
         {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            Articulo nuevo = new Articulo();
+            nuevo.Codigo = articulo.Codigo;
+            nuevo.Nombre = articulo.Nombre;
+            nuevo.Descripcion = articulo.Descripcion;
+            nuevo.Marca = new Marca { Id = articulo.IdMarca };
+            nuevo.Categoria = new Categoria { Id = articulo.IdCategoria };
+            //tengo dudas con esto, a chequear
+            nuevo.Imagenes = new List<Imagen>();
+            nuevo.Id = id;
+
+            negocio.modificar(nuevo);
+
         }
 
         // DELETE: api/Articulo/5
         public void Delete(int id)
         {
+            
         }
     }
 }
