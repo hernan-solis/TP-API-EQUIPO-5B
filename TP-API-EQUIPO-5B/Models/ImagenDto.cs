268b72c3ebb05918a36efc072acdde549c729245
Hache94k <hernansolis94k@gmail.com>
2025-10-15 21:30:07 -0300

    crear ImagenDto controller post

---
+++ TP-API-EQUIPO-5B/Models/ImagenDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TP_API_EQUIPO_5B.Models
+{
+    public class ImagenDto
+    {
+        public int IdArticulo { get; set; }
+        public List<string> Imagenes { get; set; }
+    }
+}
